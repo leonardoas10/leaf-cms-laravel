6fa04378ac0ca98460c7b0f2051faff700995231
Leonardo <leonardo@Robinsons-Mac-mini.local>
2019-10-10 11:33:14 -0300

    Fixing bugs showing the posts

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+
+use Illuminate\Support\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
+
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 use \App\Category;
@@ -29,6 +33,29 @@ class AppServiceProvider extends ServiceProvider
         if (Schema::hasTable('categories')) {
             View::share('categories', Category::all());
         }
+
+          /**
+         * Paginate a standard Laravel Collection.
+         *
+         * @param int $perPage
+         * @param int $total
+         * @param int $page
+         * @param string $pageName
+         * @return array
+         */
+        Collection::macro('paginate', function($perPage, $total = null, $page = null, $pageName = 'page') {
+            $page = $page ?: LengthAwarePaginator::resolveCurrentPage($pageName);
+            return new LengthAwarePaginator(
+                $this->forPage($page, $perPage),
+                $total ?: $this->count(),
+                $perPage,
+                $page,
+                [
+                    'path' => LengthAwarePaginator::resolveCurrentPath(),
+                    'pageName' => $pageName,
+                ]
+            );
+        });
         
     }
 }
